089fdd0a65ef0fb17c462f59c5a3748fbd94942a
Sushmita Adannavar <sushmitaadannavar90@gmail.com>
2023-07-25 17:47:58 +0530

    Creating AdressBook class

---
+++ AdressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdressbookSystem
+{
+    public class AdressBook
+    {
+       // List<AdressBook> details = new List<AdressBook>();
+        public string FirstName
+        {get;set;}
+        public string LastName
+        { get; set; }
+        public string adress
+        { get; set; }
+        public string city
+        { get; set; }
+        public string state
+        { get; set; }
+        public string zipcode
+        { get; set; }
+        public string phonenum
+        { get; set; }
+        public AdressBook(string firstName,string lastName,string adress,string city,string state, string zipcode,string phonenum)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.adress = adress;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.phonenum = phonenum;
+        }
+       
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace AdressbookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value");
+            Console.WriteLine("1.Add person to adressbook 2.Edit existing person 3.Delete person 4.Add multiple persons");
+            int ch =Convert.ToInt32(Console.ReadLine());
+            switch(ch)
+            {
+                case 1:Contact ab = new Contact();
+                      ab.CreatePerson();
+                      break;
+                case 3:Contact ab1 = new Contact(); 
+                      ab1.RemovePersonFromList();
+                      break;
+
+            }
+        }
+    }
+}
